a6cf94d1b061b358dedc4df0755f839f7944289f
Neyzimho <flasney.medeiros@outlook.com>
2026-08-22 01:21:20 -0300

    feat: created Get and Delete user requisitions methods

---
+++ src/main/java/com/neyzimho/user/infrastructure/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.neyzimho.user.infrastructure.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
