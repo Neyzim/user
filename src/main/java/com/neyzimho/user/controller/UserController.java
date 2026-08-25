552f04dfb5890d5b052b409dbfe4c15875573293
Neyzimho <flasney.medeiros@outlook.com>
2026-08-25 14:07:05 -0300

    fix: restaura arquivos perdidos devido a conflitos e revert anterior

--- src/main/java/com/neyzimho/user/controller/UserController.java
+++ src/main/java/com/neyzimho/user/controller/UserController.java
@@ -26,7 +26,6 @@ public class UserController {
         return ResponseEntity.ok(userService.saveUser(userDto));
     }
 
-
     @PostMapping(value = "/login")
     public String login(@RequestBody UserDto userDto){
         Authentication auth = authenticationManager.authenticate(
